f1ffc803f6dc5bc27c36580636f8dcb5f4448cf2
Rodrigo Acevedo <raacevedof@gmail.com>
2026-03-28 21:35:29 +0100

    refactor(migration): change id and categoryId types in news_articles table

--- packages/database/migrations/20260328185454_add-news-articles/up.sql
+++ packages/database/migrations/20260328185454_add-news-articles/up.sql
@@ -3,15 +3,17 @@
 -- Created: 2026-03-28T18:54:54.922Z
 
 CREATE TABLE news_articles (
-  id            UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  id            TEXT NOT NULL DEFAULT gen_random_uuid()::TEXT,
   title         TEXT NOT NULL,
   description   TEXT NOT NULL,
   url           TEXT NOT NULL UNIQUE,
   "imageUrl"    TEXT,
   source        TEXT NOT NULL,
   "publishedAt" TIMESTAMPTZ NOT NULL,
-  "categoryId"  UUID NOT NULL REFERENCES categories(id),
-  "createdAt"   TIMESTAMPTZ NOT NULL DEFAULT now()
+  "categoryId"  TEXT NOT NULL REFERENCES categories(id),
+  "createdAt"   TIMESTAMPTZ NOT NULL DEFAULT now(),
+
+  CONSTRAINT "news_articles_pkey" PRIMARY KEY ("id")
 );
 
 CREATE INDEX idx_news_articles_category ON news_articles("categoryId");
