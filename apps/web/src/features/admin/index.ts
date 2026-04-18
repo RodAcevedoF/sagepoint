270465bc0deffed0462521deb35b45dd4967e33a
Rodrigo Acevedo <raacevedof@gmail.com>
2026-04-19 00:17:02 +0200

    fix: correct import path for AdminRoadmapsTable component

--- apps/web/src/features/admin/index.ts
+++ apps/web/src/features/admin/index.ts
@@ -3,6 +3,6 @@ export { AdminStatsCards } from "./components/Cards/AdminStatsCards";
 export { AdminUsersTable } from "./components/AdminUsers/AdminUsersTable";
 export { AdminSystemHealth } from "./components/AdminHealth/AdminSystemHealth";
 export { AdminQueueStats } from "./components/AdminQueue/AdminQueueStats";
-export { AdminRoadmapsTable } from "./components/AdminRoadmapsTable";
+export { AdminRoadmapsTable } from "./components/AdminRoadmaps/AdminRoadmapsTable";
 export { AdminDocumentsTable } from "./components/AdminDocuments/AdminDocumentsTable";
 export { AdminAnalytics } from "./components/AdminAnalytics/AdminAnalytics";
